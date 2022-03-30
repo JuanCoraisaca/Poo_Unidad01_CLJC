124e750b05ecd14eaca55a0663e797fe44af0e47
PC-12 <PC-12@DESKTOP-4UM0NTN>
2022-03-30 13:36:36 +0200

    Cambio el mensaje de hola mundo al español

---
+++ src/main/java/ups/edu/ec/poo_unidad01_cljc/Poo_Unidad01_CLJC.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.poo_unidad01_cljc;
+
+/**
+ *
+ * @author PC-12
+ */
+public class Poo_Unidad01_CLJC {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        
+    }
+}
