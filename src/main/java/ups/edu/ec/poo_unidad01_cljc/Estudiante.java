0b5987599e5dcdb6add608c279424de6b94f0ab3
PC-12 <PC-12@DESKTOP-4UM0NTN>
2022-03-30 14:29:55 +0200

    Clase estudiante y metodos

---
+++ src/main/java/ups/edu/ec/poo_unidad01_cljc/Estudiante.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo_unidad01_cljc;
+
+/**
+ *
+ * @author PC-12
+ */
+public class Estudiante {
+    String nombre;
+    int calificacion;
+    String carrera;
+    double peso;
+    
+}
