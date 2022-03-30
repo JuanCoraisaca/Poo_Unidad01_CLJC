aeff19541e914acd654fa7888b59ea4491b16866
PC-12 <PC-12@DESKTOP-4UM0NTN>
2022-03-30 14:49:04 +0200

    Nueva clase de transporte

---
+++ src/main/java/ups/edu/ec/poo_unidad01_cljc/Transporte.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo_unidad01_cljc;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PC-12
+ */
+public class Transporte {
+    String marca;
+    String placa;
+    Date año;
+    double precio;
+    int valoracion;
+    
+    
+    
+    
+    
+}
